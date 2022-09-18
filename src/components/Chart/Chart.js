6eb3e34bb764f089d323d3dbb26b1241006b7de7
nolann7 <nolann709@gmail.com>
2022-09-18 12:28:12 +0300

    chart add

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+function Chart(props) {
+  const expensesArrayValues = props.dataPoints.map(
+    dataPoint => dataPoint.value,
+  );
+  let totalMax = Math.max(...expensesArrayValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map(dataPoint => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMax}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Chart;
